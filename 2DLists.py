baa21333e5e6e94dcc662925735dcc6e2a763237
Wilfredo Joshua Robinson Moore <wilfredo-joshua.robinson-moore@eurecom.fr>
2023-09-06 13:30:04 -0400

    New story for this one too!

--- 2DLists.py
+++ 2DLists.py
@@ -1,17 +1,16 @@
-# this is an example of a 2D lists
-# 2D lists --> lists inside lists
+# 2D lists are a somewhat simplistic way of expressing 2D matrices in Python. Other ways for matrix operations will be used later on in the lab! (Numpy Arrays!)
+# If you want to look at them as a table, think of each list as a "column" and each element in the list as an element in a "row"
+# As an example, let's create a matrix that gives each of the member in this group a random feature.
+# The first column is "Group Member", the second is "Height", and the third is "Weight". Since there are 4 lab members (rows), and three categories (Name, Height, Weight, the columns!), then this is a 4x3 table! (it's just one potential interpretation of a 2D list )
+table = [["Namrata", "Brayn", "Tai", "Wilfredo"], [160, 170, 190, 180], [100, 200, 200, 300]]
+print("The full table is:")
+print(table)
 
-# lets create a 3*3 matrix using 2D lists
+# Now if we want to access individual elements in this "table":
+# We can access the element in the first row and first column (indices start from 0)
+print("\nThe element in the first row, first column is:")
+print(table[0][0])
 
-matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-print(matrix)
-# this matrix has 3 rows and 3 columns
-
-# we can access individual elements within a 2D list using the row and column indices 
-# the indices start from 0 
-
-# we want to access the element in the first row and first column:
-print(matrix[0][0])
-
-# how about in the second row and third column?
-print(matrix[1][2])
+# And another example:
+print("\nAnd now in the second column, last row")
+print(table[1][3])
