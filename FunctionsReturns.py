afb32cac3898df9d527e643b7d04b82efde85bff
longtaichen <longtai.chen@slu.edu>
2023-09-11 11:04:58 -0500

    FunctionsReturns.py update

--- FunctionsReturns.py
+++ FunctionsReturns.py
@@ -1,3 +1,4 @@
+# we can import math to get some other math function in Python 
 import math as m
 
 # this function will get the radius of the wheel and returns the area of the wheel
@@ -8,4 +9,9 @@ def calc_wheel_area(radius):
 # now we want to call this function
 wheel_radius = 0.2 # 0.2 meters
 wheel_area = calc_wheel_area(wheel_radius)
+
+# we can also use "round" to assign the decimals for float
+wheel_area_two_decimals = round(wheel_area,2)
+
 print("The area of the wheel is", wheel_area)
+print("The area of the wheel in two decimals is",wheel_area_two_decimals)
