32726421015709b4bc2ac294844b2b156a512556
longtaichen <longtai.chen@slu.edu>
2023-09-11 14:55:06 -0500

    try_except.py_update

--- try_except.py
+++ try_except.py
@@ -1,23 +1,19 @@
-# # Divide by Zero
-# def divide_numbers(x, y):
-#     try:
-#         return x / y
-#     except ZeroDivisionError:
-#         print("Division by zero error")
-
-# print(divide_numbers(10, 2))
-# print(divide_numbers(10, 0))
-
 # Try Except
+# try and except can help us handle the expceptions that might occur during execution
+
+# first we define a function called divide_numbers
 def divide_numbers(x,y):
+    # if there is no error during execution of this code, it will return x/y 
     try:
         return x/y
+    # This except handle the zero division error which will happen when the value divide by zero. if ZeroDivisionError occur, it will return the message Division by zero error 
     except ZeroDivisionError:
         return "Division by zero error"
+    # This will handle other input error, and return the message invalid 
     except:
         return "Invalid"
 
-# here we also handled the invalid input error as well. so depending on the situation we can handle the error
-
+# now we can try how the function works
 print(divide_numbers(5,2))
-print(divide_numbers(5,0))
+print(divide_numbers(0,0))
+print(divide_numbers('0',0))
